60b8d074dd403415ddcdb9df40a4b2f976976097
Ktioru <mateuscst45@gmail.com>
2023-06-17 18:46:52 -0300

    Added basic tic tac toe system

--- script.js
+++ script.js
@@ -1,11 +1,63 @@
 const board = document.querySelector(".board")
-console.log(board)
 
-function createSquares() {
-    console.log("I'm ALIVE")
+const squares = {}
 
-    let square = document.createElement("div")
-    square.classList.add("square")
-    board.appendChild(square)
+let moveCount = 0
+
+const Player = (name, move) => {
+    const turn = (square) => {
+        if (squares[`square ${square}`].innerText == "" ) {
+            squares[`square ${square}`].innerText = move
+            moveCount++
+        }
+        
+        
+    }
+    return {turn}
+}
+
+const playerOne = Player("Mateus", "X")
+const playerTwo = Player("SomeOne", "O")
+
+
+
+const game = {
+    createSquares: () => {
+            
+            
+            for (let c = 1; c < 10; c++) {
+                let square = document.createElement("div")
+
+                square.classList.add("square")
+
+                square.addEventListener("click", () => {
+                    if (moveCount % 2 == 0) {
+                        playerOne.turn(c)
+                    } else {
+                        playerTwo.turn(c)
+                    }
+                   
+                })
+                
+                squares[`square ${c}`] = square
+                
+
+
+                board.appendChild(square)
+            }
+            console.log(squares)
+    },
+    
+    
 
 }
+
+
+game.createSquares()
+
+
+
+
+
+
+
