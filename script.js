e0dbea0137b83aa469a83e0bd1347b5e30b013d5
Ktioru <mateuscst45@gmail.com>
2023-06-17 20:49:49 -0300

    Made the game entirely functional

--- script.js
+++ script.js
@@ -4,9 +4,17 @@ const squares = {}
 
 let moveCount = 0
 
+let start = false
+
+let win = false
+
+const gameTitle = document.querySelector(".game-title")
+
+const button = document.querySelector(".button")
+
 const Player = (name, move) => {
     const turn = (square) => {
-        if (squares[`square ${square}`].innerText == "" ) {
+        if (squares[`square ${square}`].innerText == ""  && start == true) {
             squares[`square ${square}`].innerText = move
             moveCount++
         }
@@ -36,6 +44,12 @@ const game = {
                     } else {
                         playerTwo.turn(c)
                     }
+                    if(moveCount > 4) {
+                        
+                        game.verifyWinner()
+                    }
+                    
+
                    
                 })
                 
@@ -47,6 +61,78 @@ const game = {
             }
             console.log(squares)
     },
+    verifyWinner: () => {
+        //Vertical Wins
+        if(squares["square 1"].innerText == squares["square 2"].innerText && squares["square 1"].innerText == squares["square 3"].innerText && squares["square 1"].innerText != "") {
+            win = true
+            console.log("WIN 1")
+        } else if (squares["square 4"].innerText == squares["square 5"].innerText && squares["square 4"].innerText == squares["square 6"].innerText && squares["square 4"].innerText != "") {
+            win = true
+            console.log("WIN 2")
+
+        } else if (squares["square 7"].innerText == squares["square 8"].innerText && squares["square 7"].innerText == squares["square 9"].innerText && squares["square 7"].innerText != "") {
+            win = true
+            console.log("WIN 3")
+        } 
+        //HorizontalWins
+        else if (squares["square 1"].innerText == squares["square 4"].innerText && squares["square 1"].innerText == squares["square 7"].innerText && squares["square 1"].innerText != "") {
+            win = true
+            console.log("WIN 4")
+
+        }
+        else if (squares["square 2"].innerText == squares["square 5"].innerText && squares["square 2"].innerText == squares["square 8"].innerText && squares["square 2"].innerText != "") {
+            win = true
+            console.log("WIN 5")
+
+        }
+        else if (squares["square 3"].innerText == squares["square 6"].innerText && squares["square 3"].innerText == squares["square 9"].innerText && squares["square 3"].innerText != "") {
+            win = true
+            console.log("WIN 6")
+
+        }
+        //Other Wins
+        else if (squares["square 1"].innerText == squares["square 5"].innerText && squares["square 1"].innerText == squares["square 9"].innerText && squares["square 1"].innerText != "") {
+            win = true
+            console.log("WIN 7")
+
+        } else if (squares["square 3"].innerText == squares["square 5"].innerText && squares["square 3"].innerText == squares["square 7"].innerText && squares["square 3"].innerText != "") {
+            win = true
+            console.log("WIN 8")
+
+        }
+        
+        if(moveCount == 9 && win == false) {
+            button.innerText = "Restart"
+            gameTitle.innerText = "It's a Tie!"
+            
+        } else if (win == true) {
+            button.innerText = "Restart"
+
+            if(moveCount % 2 != 0) {
+                gameTitle.innerText = "Player One Wins!"
+            } else {
+                gameTitle.innerText = "Player Two Wins!"
+            }
+            
+        }
+        
+    },
+    startRestart: () => {
+        if (start == false) {
+            start = true
+
+        } else if(start == true) {
+            button.innerText = "Start"
+            win = false
+            moveCount = 0
+            gameTitle.innerText = "Let's Play!"
+            for (let c = 1; c < 10; c++) {
+                squares[`square ${c}`].innerText = ""
+            }
+            
+        }
+        
+    }
     
     
 
